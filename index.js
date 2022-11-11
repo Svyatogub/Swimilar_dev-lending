cb2c028a5b7e09c259e29197f9101df4de82948f
Oleksandr Vladymyrov <vladymyrovalexandr97@gmail.com>
2022-11-11 18:46:07 +0200

    fixes

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-function navigation () {
-    if(document.referrer !== "./quiz/quiz.html") {
-        return window.history.back()
-    } else {
-        return window.location.href = './quiz/quiz.html'
-    }
-    
+(function firstLoad() {
+    sessionStorage.setItem("executed", true)
+})();
+// console.log(location.host)
+
+function mainPageHandler() {
+    sessionStorage.setItem("page", "start");
+    window.location.href = "/index.html"
 }
-console.log(document.referrer)
+function firstPageHandler() {
+    sessionStorage.setItem("page", "quiz");
+    sessionStorage.setItem("forQuiz", "quiz");
+    sessionStorage.removeItem("forReg")
+    window.location.href = "/quiz/quiz.html"
+}
+
+function secondPageHandler() {
+    sessionStorage.setItem("page", "registration");
+    sessionStorage.setItem("forReg", "reg")
+    sessionStorage.removeItem("forQuiz")
+    window.location.href = "/registration/registration.html"
+}
+
+ function registrationHandler() {
+    let loaded = sessionStorage.getItem("executed")
+    let lastPage = sessionStorage.getItem("page");
+    let qPage = sessionStorage.getItem("forQuiz");
+    let rPage = sessionStorage.getItem("forReg");
+    if(loaded) {
+        firstPageHandler()
+    }
+    if(lastPage == "start" && qPage == "quiz") {
+        firstPageHandler()
+    }
+    if(lastPage == "start" && rPage == "reg") {
+        secondPageHandler()
+    }
+ }
